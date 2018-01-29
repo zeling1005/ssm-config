8a9037c8d741cec806e8d0094d0f75d8f1c43d13
Peter <neu20133788@163.com>
2018-01-29 23:32:36 +0800

    主题配置，初版

---
+++ src/main/java/xyz/zeling/ssmconfig/modules/theme/ThemeController.java
@@ -0,0 +1,32 @@
+package xyz.zeling.ssmconfig.modules.theme;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @description TODO
+ * @author zeling
+ * @date 2018年1月29日 下午8:32:30
+ */
+@Controller
+// @RequestMapping("/theme")
+public class ThemeController {
+    @RequestMapping("/main")
+    public ModelAndView main() {
+        return new ModelAndView("/theme/main");
+    }
+
+    @RequestMapping("/form")
+    public ModelAndView user() {
+        return new ModelAndView("/theme/form", "user", new User());
+    }
+
+    @RequestMapping("/result")
+    public ModelAndView processUser(User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("u", user);
+        modelAndView.setViewName("/theme/result");
+        return modelAndView;
+    }
+}
