bc084dee2f95b9b18210a644ab091d2d3ed2ddc0
Peter <neu20133788@163.com>
2018-02-10 01:07:22 +0800

    添加index.jsp

---
+++ src/main/java/xyz/zeling/ssmconfig/modules/welcome/WelcomeController.java
@@ -0,0 +1,20 @@
+package xyz.zeling.ssmconfig.modules.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @description TODO
+ * @author zeling
+ * @date 2018年2月10日 上午12:27:34 
+ */
+@Controller
+@RequestMapping("welcome")
+public class WelcomeController {
+    
+    @RequestMapping("main")
+    public ModelAndView toMain() {
+        return new ModelAndView("welcome/main");
+    }
+}
