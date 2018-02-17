5e07542866593b026e98fdd6139152a2ffa5a71a
Peter <neu20133788@163.com>
2018-02-18 01:38:19 +0800

    添加App.class和项目说明

---
+++ src/main/java/xyz/zeling/ssmconfig/app/App.java
@@ -0,0 +1,17 @@
+package xyz.zeling.ssmconfig.app;
+
+/**
+ * @description app类
+ * @author zeling
+ * @date 2018年2月18日 上午12:34:15 
+ */
+public class App {
+    /**
+     * @description TODO
+     * @date 2018年2月18日 上午12:34:16
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+}
