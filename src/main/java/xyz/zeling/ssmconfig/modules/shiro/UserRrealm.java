60f016deb4e3467034e3126047c31d34d9d9c560
Peter <neu20133788@163.com>
2018-03-02 01:59:40 +0800

    shiro用户修改权限后，没有退出时，需要清空缓存

--- src/main/java/xyz/zeling/ssmconfig/modules/shiro/UserRrealm.java
+++ src/main/java/xyz/zeling/ssmconfig/modules/shiro/UserRrealm.java
@@ -1,5 +1,6 @@
 package xyz.zeling.ssmconfig.modules.shiro;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -31,9 +32,21 @@ public class UserRrealm extends AuthorizingRealm {
         // 可使用缓存
         SimpleAccount accountInfo = new SimpleAccount(token.getPrincipal(), token.getCredentials(), "zeling");
         Object principal = token.getPrincipal();
-        if("zeling".equals(String.valueOf(principal))) {
+        if ("zeling".equals(String.valueOf(principal))) {
             accountInfo.setCredentials("1678fff13f3a9b70308bcde3afdad0e8");
         }
         return accountInfo;
+    }
+
+    /**
+     * 如果用户正常退出，缓存自动清空。 如果用户非正常退出，缓存自动清空。
+     * 如果修改了用户的权限，而用户不退出系统，修改的权限无法立即生效，需要手动进行编程实现： 在权限修改后调用realm的clearCache方法清除缓存。
+     * 正常开发时要放在service中调用。
+     *
+     * @date 2018年3月2日 上午1:54:29
+     */
+    public void clearCached() {
+        PrincipalCollection principals = SecurityUtils.getSubject().getPrincipals();
+        super.clearCache(principals);
     }
 }
